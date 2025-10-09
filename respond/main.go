8d7feb697f24bd8b33d7e56d7896c2df01edc44e
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-09 09:46:32 +0200

    respond

---
+++ respond/main.go
@@ -0,0 +1,12 @@
+package respond
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
