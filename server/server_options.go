ce75acdb27085b6abe28bd4e461b8fdb83a49eec
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-18 19:16:03 +0200

    server options

--- server/server_options.go
+++ server/server_options.go
@@ -1,8 +1,36 @@
 package server
 
+type ServerOptionName string
+
+const (
+	TRANSLATIONS_ENABLED ServerOptionName = "translations_enabled"
+	TRANSLATIONS_ADD     ServerOptionName = "translations_add"
+	TRANSLATION_DEFAULT  ServerOptionName = "translation_default"
+)
+
 type ServerOption struct {
+	Name     ServerOptionName
+	Value    string
+	Filename string
 }
 
 func EnableTranslations() ServerOption {
-	return ServerOption{}
+	return ServerOption{
+		Name: TRANSLATIONS_ENABLED,
+	}
+}
+
+func AddTranslationFile(short, filepath string) ServerOption {
+	return ServerOption{
+		Name:     TRANSLATIONS_ADD,
+		Value:    short,
+		Filename: filepath,
+	}
+}
+
+func SetDefaultLanguage(short string) ServerOption {
+	return ServerOption{
+		Name:  TRANSLATION_DEFAULT,
+		Value: short,
+	}
 }
